c6836828372ee6bdd9d60e21d35503c4e8b81255
Ronnie Nigash <nigash@uw.edu>
2015-06-30 14:36:39 -0700

    Add histogram creation macro

---
+++ histogram.C
@@ -0,0 +1,16 @@
+#include "Riostream.h"
+void histogram() {
+	// Read data from ASCII file and create histogram/ntuple combo
+	ifstream in;
+ 	in.open("/users/ronnie/git/Journal-Analysis/data.txt");
+
+ 	Float_t month, date, day, words;
+	TFile *file = new TFile("histogramTest.root", "CREATE");
+	TH1F *histo = new TH1F("histo", "writing distribution", 100, 0, 2);
+	TTree *Tree = new TTree("ntuple", "data from file");
+					
+	Tree->ReadFile(Form("/users/ronnie/git/Journal-Analysis/data.txt"), "month:date:day:words");
+ 	Tree->Draw("words");
+	Tree->Write();
+	
+}
