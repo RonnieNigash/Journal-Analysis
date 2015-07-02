0f48ea145de405e35dd8fbb33ad45a43e79fbc6a
Ronnie Nigash <nigash@uw.edu>
2015-07-02 15:33:44 -0700

    Analyze! Build a scatter plot of data points and save .gif using C++ ROOT framework

--- histogram.C
+++ histogram.C
@@ -10,10 +10,10 @@ void histogram() {
 	TH1F *histo = new TH1F("histo", "writing distribution", 100, 0, 2);
 	TTree *Tree = new TTree("ntuple", "data from file");
 					
-	Tree->ReadFile(Form("/users/ronnie/git/Journal-Analysis/data.txt"), "month:date:day:words");
-	graph = new TGraph(170, &day, &words);
-	graph->Draw("A");
-//	Tree->Draw("day:words");
+	Tree->ReadFile(Form("/users/ronnie/git/Journal-Analysis/Daily Journals/data.txt"), "month:date:day:words");
+	graph = new TGraph(170, &words, &day);
+//	graph->Draw("A");
+	Tree->Draw("words:day");
 //	Tree->Fit("day", "words");
 	Tree->Write();
 	
