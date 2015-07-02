2bc2f68fabdb5653b6271e1423488ac4401760aa
Ronnie Nigash <nigash@uw.edu>
2015-07-02 14:10:24 -0700

    Remove white spaces to correct dictionary mapping

--- readWriteData.py
+++ readWriteData.py
@@ -13,12 +13,12 @@ for file in glob.glob("*.rtf"):
 
 	print(first_line)
 	print(end_line)
-	month = file[5:-10] # String
+	month = file[5:-12].strip() # String
 	month_dict = {"Jan" : 1, "Feb" : 2, "Mar" : 3, "Apr" : 4, "May" : 5, "June" : 6, "July" : 7, "Aug" : 8, "Sep" : 9, "Oct" : 10, "Nov" : 11, "Dec" : 12}
-	month = month_dict[month]
-	print("The month # is: " + month)
+	month = month_dict[month] # Integer
+	print(month)
 	# Format: "Month Date Day Words"
 #	new_file.write(file[5:-10] + " " + first_line[4:8] + " " + end_line + "\n")
-	new_file.write(month + " " + first_line[4:8] + " " + end_line + "\n")
+	new_file.write(str(month) + " " + first_line[4:8] + " " + end_line + "\n")
 
 new_file.close()
