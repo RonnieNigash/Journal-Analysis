ad1687edd7bd434887f5d9511e4739ad82b3e5a5
Ronnie Nigash <nigash@uw.edu>
2015-07-02 14:15:47 -0700

    Remove date from data.txt output file

--- readWriteData.py
+++ readWriteData.py
@@ -17,7 +17,7 @@ for file in glob.glob("*.rtf"):
 	month_dict = {"Jan" : 1, "Feb" : 2, "Mar" : 3, "Apr" : 4, "May" : 5, "June" : 6, "July" : 7, "Aug" : 8, "Sep" : 9, "Oct" : 10, "Nov" : 11, "Dec" : 12}
 	month = month_dict[month] # Integer of 1, 2, ..., 12
 	print(month)
-	# Format: "Month Date Day Words" -> All integers concatenated to string
+	# Format: "Month Day Words" -> All integers concatenated to string
 	new_file.write(str(month) + " " + first_line[4:8] + " " + end_line + "\n")
 
 new_file.close()
