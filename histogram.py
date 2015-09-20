85d6e111ce6662973ab7806bd57b0726942e0e8b
Ronnie Nigash <nigash@uw.edu>
2015-09-19 18:36:20 -0700

    Update data generation and sorting

--- histogram.py
+++ histogram.py
@@ -1,14 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys, os
 
-with open("/Daily Journals/data.txt") as f:
-	data = f.read()
 
-data = data.split('\n')
-data = data.pop(0) # get rid of words at top
-
-days = [row.split(' ')[2] for row in data] # month, date, days, words is format
-words = [row.split(' ')[-1] for row in data]
-
-graph = plt.figure()
+data = np.loadtxt((sys.path[0] + "/Daily Journals/data.txt"), comments='#', skiprows=1, usecols=(2,3))
+data = data.tolist()
+data.sort()
 
