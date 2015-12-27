fd71650b1b7923037ff19e1da85a1056b4d097fa
Ronnie Nigash <nigash@uw.edu>
2015-12-27 10:45:11 -0800

    Comb through files, append contents to new file

---
+++ Daily Journals/allWords.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Remove spaces from filenames for easy process
+for f in *\ *; do mv "$f" "${f// /_}"; done
+
+# Append contest of *.html and *.rtf files to new file
+for file in *.html *.rtf
+do
+	echo "Processing $file..."
+	cat $file >> fileContexts.txt
+done
